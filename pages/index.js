dd3a2864948f93817fbe55809b9226e4e9983580
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-10-12 19:24:11 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-import Image from "next/image";
+import layout from "../components/layout";
 
 export default function Home() {
-  return <div>Hello Next.js</div>;
+  return (
+    <div>
+      <layout page="Crypto watch - Acceuil">
+        <h1>Mon app avec Next.js</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
+          distinctio nisi adipisci. At dolor illum inventore id veniam rem
+          corrupti ipsa commodi, est vel sunt soluta ad. Recusandae ullam
+          placeat omnis in at quisquam saepe fugiat adipisci cum, aspernatur
+          quasi ducimus, totam quibusdam odio aliquid suscipit! Consectetur
+          rerum odit atque, voluptas eaque in odio repellat quam distinctio
+          molestiae fugiat quisquam est et incidunt dolor libero corrupti
+          excepturi laborum quas earum voluptates placeat at ex id? Omnis illum
+          iure voluptatibus nostrum aut est architecto obcaecati? Eveniet,
+          aperiam. Asperiores tempora optio repellat sint, illum assumenda
+          deserunt dicta unde aspernatur eius. Odio, quis.
+        </p>
+      </layout>
+    </div>
+  );
 }
